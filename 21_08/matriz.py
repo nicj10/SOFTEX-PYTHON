aba368d0c03608ae311507d07108eff8c4fccf27
nicj10 <167533566+nicj10@users.noreply.github.com>
2025-08-21 19:53:33 +0000

    matriz

---
+++ 21_08/matriz.py
@@ -0,0 +1,21 @@
+"""matriz = [[i for i in range(3)] for x in range(3)]
+
+print(matriz)
+"""
+"""
+matriz2 = []
+
+for i in range(3):
+    sublista = []
+    matriz2.append(sublista)
+    for x in range(3):
+        sublista.append(x)
+        
+print(matriz2[1][2])
+"""
+
+condominio = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], 
+              [[1, 2, 3], [4, 5, 6], [7, 8, 9]], 
+              [[1, 2, 3], [4, 5, 6], [7, 8, 9]]]
+
+print(condominio[1][2])
