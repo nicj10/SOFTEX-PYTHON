19cf168302a9f0bdf6e0df99fbb27dc35df133ab
nicj10 <167533566+nicj10@users.noreply.github.com>
2025-10-28 17:52:33 +0000

    nao to entendendo, vou estudar dps

---
+++ Banco de dados/28_10/sqlite_python.py
@@ -0,0 +1,10 @@
+import sqlite3
+from pathlib import Path
+'''
+BASE_DIR = Path(__file__).resolve().parent
+print(BASE_DIR)
+
+DB_PATH = BASE_DIR / "db" /
+"escola_v2.db"
+'''
+db_connection = sqlite3.connect()
